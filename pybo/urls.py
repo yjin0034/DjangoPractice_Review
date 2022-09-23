19997d542d9a230599ef91a10f803dad052544ce
yjin0034 <yjin0034@naver.com>
2022-09-23 19:13:51 +0900

    URL 별칭, URL 네임스페이스 적용(pybo.urls.py, templates/question_list.html)

--- pybo/urls.py
+++ pybo/urls.py
@@ -9,14 +9,19 @@ from django.urls import path
 from . import views
 
 
+# URL 네임스페이스 설정
+app_name = 'pybo'
+
 urlpatterns = [
     # index 페이지 URL 매핑
     # path('') : '' 이 사용되었다. 이렇게 되는 이유는 config/urls.py 파일에서 이미 pybo/로 시작하는 URL이 pybo/urls.py 파일과 먼저 매핑되었기 때문
-    path('', views.index),
+    # name='index' : 해당 URL에 대해 URL 별칭 설정
+    path('', views.index, name='index'),
     # 질문 상세 페이지 URL 매핑
     # <int:question_id> : 정수형 숫자 매핑
     # 예를 들어, 만일 http://localhost:8000/pybo/2/ 페이지가 요청되면
     # 여기에 등록한 매핑 룰에 의해 http://localhost:8000/pybo/<int:question_id>/ 가 적용되어,
     # question_id 에 2가 저장되고, views.detail 함수도 실행
-    path('<int:question_id>/', views.detail),
+    # name='detail' : 해당 URL에 대해 URL 별칭 설정
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
