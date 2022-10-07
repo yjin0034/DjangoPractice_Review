b630796c234a8957d2d6c9668cab5fd924d13c4e
yjin0034 <yjin0034@naver.com>
2022-10-07 22:33:54 +0900

    질문, 답변 글에 마크다운 문법 적용 가능하도록 기능 추가 - 템플릿 필터(pybo/templatetags/pybo_filter.py), Template(templates/pybo/question_detail.html) 관련 코드 수정 및 추가

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,8 +1,9 @@
 # 템플릿 필터 직접 작성
 
-# 빼기 기능 관련 템플릿 필터 직접 작성  #  |sub:[숫자] 와 같은 기능
 
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 
 register = template.Library()
@@ -15,3 +16,14 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+# 마크다운 관련 필터
+# mark 함수 : markdown 모듈과 mark_safe 함수를 이용하여 입력 문자열을 HTML로 변환하는 필터 함수
+@register.filter
+def mark(value):
+    # nl2br, fenced_code : 마크다운 확장 기능
+    # nl2br : 줄바꿈 문자를 <br> 로 바꾸어 주는 기능. nl2br을 사용하지 않을 경우, 줄바꿈을 하기 위해서는 줄 끝에 스페이스(' ')를 두개 연속으로 입력해야 함.
+    # fended_code : 마크다운의 소스코드 표현을 위해 필요
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
